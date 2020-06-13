e9f131ecda10438b4e5399132f0c33f0fe6cb860
AwradArrakAlanteri-77 <aw.ala@yahoo.com>
2020-06-13 19:17:35 +0300

    Part one is done .👍🏻

---
+++ CW-1-AwradArrakAlanteri-77.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let name = "Awrad"
+var hoppy1 = "Riding horses"
+var hoppy2 = "Reading books"
+var age = 18
+var height = 164
+
+print(name)
+print(age)
+print(height)
+print(hoppy1)
+print(hoppy2)
