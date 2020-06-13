abc10877db23b2b43bbe12fa823db5df0fb13d37
AwradArrakAlanteri-77 <aw.ala@yahoo.com>
2020-06-13 23:04:25 +0300

    Class work is done.👍🏻

--- CW-1-AwradArrakAlanteri-77.playground/Contents.swift
+++ CW-1-AwradArrakAlanteri-77.playground/Contents.swift
@@ -4,10 +4,23 @@ let name = "Awrad"
 var hoppy1 = "Riding horses"
 var hoppy2 = "Reading books"
 var age = 18
-var height = 164
+var height = 1.64
 
 print(name)
 print(age)
 print(height)
 print(hoppy1)
 print(hoppy2)
+
+let friendname = "Fajer"
+var friendhoppy1 = "Reading books"
+var friendhoppy2 = "Shooting"
+var friendage = 17
+var friendheight = 1.59
+
+var ageDifference = age > friendage
+var nameDifference = name != friendname
+var hoppy1difference = hoppy1 != friendhoppy1
+var hoppy2Difference = hoppy2 != friendhoppy2
+var heightDifference = height > friendheight
+
